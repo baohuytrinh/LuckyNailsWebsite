769d8cbb5dfed2be62bb5e758a24fc322c38e308
Bao-Huy Trinh <anthonytrinh0508@gmail.com>
2025-06-18 18:52:33 -0400

    6/18

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+let section = document.querySelector('section');
+
+
+menuToggle.onclick = function(){
+    header.classList.toggle('active')
+    section.classList.toggle('active')
+}
